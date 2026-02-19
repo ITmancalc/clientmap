10beeb3391c0e1d59b47d2f341fef8ab08275f85
ITmancalc <bonvida21@gmail.com>
2026-02-19 08:00:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // Put your *current* Apps Script WebApp /exec URL here after each deployment.
 // Example:
 // window.GS_WEBAPP_URL = "https://script.google.com/macros/s/XXXXX/exec";
-window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyCADFhlxdqL5Eg4utMZTvBxTuA2ZHkPmTO6thytaF672ViyO1MBbR7m-GST52bjLmO/exec";
+window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwbrfWknpKxNhMLSvadmOYImOPTPwOQVUYbZ1rdw359e4aq1srw7MRDlHEzfkLiQS_Jyg/exec";
