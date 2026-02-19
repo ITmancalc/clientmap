4e095c8970d8148133c09a9b848e5c79f528f42f
ITmancalc <bonvida21@gmail.com>
2026-02-19 07:59:35 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // Put your *current* Apps Script WebApp /exec URL here after each deployment.
 // Example:
 // window.GS_WEBAPP_URL = "https://script.google.com/macros/s/XXXXX/exec";
-window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzJTwrX6N_O5nRlou7hnPb32R05LvS0QaTEs2sam7KvIB_WCAs36fCs2XlmIrY60fw-GQ/exec";
+window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyCADFhlxdqL5Eg4utMZTvBxTuA2ZHkPmTO6thytaF672ViyO1MBbR7m-GST52bjLmO/exec";
