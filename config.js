2de882f81d1483216a49dc25011e43fb2a513acf
ITmancalc <bonvida21@gmail.com>
2026-02-19 12:27:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 // IMPORTANT:
 // Put your *current* Apps Script WebApp /exec URL here after each deployment.
 // Example:
-// window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwbrfWknpKxNhMLSvadmOYImOPTPwOQVUYbZ1rdw359e4aq1srw7MRDlHEzfkLiQS_Jyg/exec";
-window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwbrfWknpKxNhMLSvadmOYImOPTPwOQVUYbZ1rdw359e4aq1srw7MRDlHEzfkLiQS_Jyg/exec";
+// window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyglcDYAN25D0F39tSkdeAymaz73NAamXJkt7PPy7PtoByaEcbPlrrBlTBaPGIeh-irLA/exec";
+window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyglcDYAN25D0F39tSkdeAymaz73NAamXJkt7PPy7PtoByaEcbPlrrBlTBaPGIeh-irLA/exec";
 
 // Backward-compatible alias used by older pages
 window.GOOGLE_SHEETS_WEBAPP_URL = window.GS_WEBAPP_URL;
