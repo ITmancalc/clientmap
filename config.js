e5f0d5ba074afbdba349dc502112c3714c6a1caf
ITmancalc <bonvida21@gmail.com>
2026-02-26 09:16:14 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -2,9 +2,10 @@
 // Update GS_WEBAPP_URL here and it will apply to all pages.
 // IMPORTANT:
 // Put your *current* Apps Script WebApp /exec URL here after each deployment.
-// Example:
-// window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwbrfWknpKxNhMLSvadmOYImOPTPwOQVUYbZ1rdw359e4aq1srw7MRDlHEzfkLiQS_Jyg/exec";
-window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwbrfWknpKxNhMLSvadmOYImOPTPwOQVUYbZ1rdw359e4aq1srw7MRDlHEzfkLiQS_Jyg/exec";
 
-// Backward-compatible alias used by older pages
+// ✅ Актуальный URL (обновляй после каждого деплоя WebApp)
+window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwtBLKDLp9Okdlm0EvcUAWb5COpdHya3YTnkJJ7Vxp7uZkuIFtFi3WFgQKnum3vPdQyeA/exec";
+
+// Backward-compatible aliases used by older pages/builds
 window.GOOGLE_SHEETS_WEBAPP_URL = window.GS_WEBAPP_URL;
+window.WEBAPP_URL = window.GS_WEBAPP_URL;
