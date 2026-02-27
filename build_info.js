acf09a525c3b9b75371877058bfe09779d61f1c2
ITmancalc <bonvida21@gmail.com>
2026-02-27 10:22:40 +0300

    Add files via upload

--- build_info.js
+++ build_info.js
@@ -1,8 +1,8 @@
 // Auto-injected build metadata (update on each deploy)
 window.ITMEN_BUILD = {
-  build: "v83",
-  built_at: "2026-02-26T14:35:27Z",
-  source: "repo_v83_final_fix"
+  build: "v88",
+  built_at: "2026-02-27T07:20:00Z",
+  source: "repo_v88_summary_loader_fix"
 };
 
 (function(){
