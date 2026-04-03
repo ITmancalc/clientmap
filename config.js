f9bfe2a2591bab797abfb0e34f3d0eb1516dc2c0
ITmancalc <bonvida21@gmail.com>
2026-04-03 09:31:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // Put your *current* Apps Script WebApp /exec URL here after each deployment.
 
 // ✅ Актуальный URL (обновляй после каждого деплоя WebApp)
-window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbx3f0lF9fHTEFbOUAbdoIjUbKNnOOnVe7GDDC4KOJ2_6HfTtUkVeIRJRWglxaHUBWv87Q/exec";
+window.GS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzuAH7ZFEM0Ho7NYzd5P_mjI5CA_IWlLuzHfTXaGlKwgK8Sx20GEz59l2cdpGIijwK0iQ/exec";
 
 // Backward-compatible aliases used by older pages/builds
 window.GOOGLE_SHEETS_WEBAPP_URL = window.GS_WEBAPP_URL;
